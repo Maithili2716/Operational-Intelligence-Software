b62142c1cccb423e158ec5fea87822a9a9428bd8
maithili <maithiliw27@gmail.com>
2026-08-09 15:46:51 +0530

    final fix

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 const VITE_API_URL = import.meta.env.VITE_API_URL ||
     "http://localhost:5000";
 
-console.log("PRODUCTION API:", API_URL);
+console.log("PRODUCTION API:", VITE_API_URL);
 export async function api(path) {
     const response = await fetch(
         `${VITE_API_URL}${path}`
